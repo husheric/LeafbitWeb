c88291be6433284c2875cf770a573a8840499dc0
helencho <helencho@ac.c4q.nyc>
2018-05-19 17:11:43 -0400

    import font awesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,38 @@ import Marketplace from './Components/Marketplace';
 import './stylesheets/nav.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      sidebar: false
+    }
+  }
+
+  handleSidebar = () => {
+    this.setState({
+      sidebar: !this.state.sidebar
+    })
+  }
+
   render() {
+    console.log(this.state)
+    const sidebarClass = this.state.sidebar ? `hidden` : `visible`
+
     return (
       <div>
         <nav className="sidebar">
-          <Link to="/">Marketplace</Link>
-          <Link to="/profile">Profile</Link>
+          <div onClick={this.handleSidebar}>
+            <i className="fas fa-caret-right"></i>
+          </div>
+          <div className={`sidebar-${sidebarClass}`}>
+            <Link to="/"><i className="fas fa-store"></i></Link>
+            <Link to="/plant"><i className="fas fa-tree"></i></Link>
+            <Link to="/profile"><i className="fas fa-user"></i></Link>
+          </div>
         </nav>
         <Switch>
           <Route exact path="/" component={Marketplace} />
+          <Route path="/plant" component={Marketplace} />
           <Route path="/profile" component={Marketplace} />
         </Switch>
       </div>
