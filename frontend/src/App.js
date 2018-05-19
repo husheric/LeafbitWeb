6b463a9f82aa8f90c58f6980a37772e012675924
helencho <helencho@ac.c4q.nyc>
2018-05-19 16:34:54 -0400

    delete unused files and install router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Homepage from './Components/Homepage';
 import Marketplace from './Components/Marketplace';
-// import logo from './logo.svg';
-// import './App.css';
 
 class App extends Component {
   render() {
