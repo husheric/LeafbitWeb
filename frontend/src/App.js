cc25bf9b17c6bd3f537766e87cb3413aeda61b5d
helencho <helencho@ac.c4q.nyc>
2018-05-19 16:51:40 -0400

    implement browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 import Homepage from './Components/Homepage';
 import Marketplace from './Components/Marketplace';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Marketplace />
+      <div>
+        <nav>
+          <Link to="/">Marketplace</Link>
+          <Link to="/profile">Profile</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Marketplace} />
+          <Route path="/profile" component={Marketplace} />
+        </Switch>
       </div>
     );
   }
