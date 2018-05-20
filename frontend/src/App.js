76cb6d53854053e60c3e9b3c3068e376caa0eef7
helencho <helencho@ac.c4q.nyc>
2018-05-20 09:10:49 -0400

    initial style on sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,20 +26,21 @@ class App extends Component {
 
     return (
       <div>
-        {/* <nav className="sidebar"> */}
-          <div className={`sidebar sidebar-${sidebarClass}`}>
-            <Link to="/"><i className="fas fa-tree fa-fw fa-3x"></i></Link>
-            <Link to="/market"><i className="fas fa-store fa-fw fa-3x"></i></Link>
-            <Link to="/map"><i className="fas fa-map-marker-alt fa-fw fa-3x"></i></Link>
-            <Link to="/profile"><i className="fas fa-user fa-fw fa-3x"></i></Link>
-          </div>
-        {/* </nav> */}
+
+        <div className={`sidebar sidebar-${sidebarClass}`}>
+          <Link to="/"><i className="fas fa-tree fa-fw fa-3x"></i></Link>
+          <Link to="/market"><i className="fas fa-store fa-fw fa-3x"></i></Link>
+          <Link to="/map"><i className="fas fa-map-marker-alt fa-fw fa-3x"></i></Link>
+          <Link to="/profile"><i className="fas fa-user fa-fw fa-3x"></i></Link>
+        </div>
+
         <div className="top">
           <div onClick={this.handleSidebar}>
             <i className="fas fa-bars fa-fw fa-3x"></i>
           </div>
-          <img src="../assets/icon.png" alt="Leafbit" />
+          <img src="../assets/icon.png" alt="Leafbit" id="top-logo" />
         </div>
+
         <Switch>
           <Route exact path="/" component={TreeProgress} />
           <Route path="/market" component={Marketplace} />
