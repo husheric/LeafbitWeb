1ad12162e406251800cce4d3970c2cd58b1f22fa
helencho <helencho@ac.c4q.nyc>
2018-05-20 08:59:53 -0400

    start on side bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,7 @@ import Marketplace from './Components/Marketplace';
 import TreeProgress from './Components/TreeProgress';
 import './stylesheets/nav.css';
 import './stylesheets/App.css';
-
 import TreeMap from './Components/TreeMap'
-// import logo from './logo.svg';
-// import './App.css';
-
 
 class App extends Component {
   constructor() {
@@ -31,22 +27,24 @@ class App extends Component {
 
     return (
       <div>
-        {/* <nav className="sidebar">
+        <nav className="sidebar">
+          <div className={`sidebar-${sidebarClass}`}>
+            <Link to="/"><i className="fas fa-tree fa-fw fa-3x"></i></Link>
+            <Link to="/market"><i className="fas fa-store fa-fw fa-3x"></i></Link>
+            <Link to="/map"><i className="fas fa-map-marker-alt fa-fw fa-3x"></i></Link>
+            <Link to="/profile"><i className="fas fa-user fa-fw fa-3x"></i></Link>
+          </div>
+        </nav>
+        <div className="top">
           <div onClick={this.handleSidebar}>
             <i className="fas fa-caret-right"></i>
           </div>
-          <div className={`sidebar-${sidebarClass}`}>
-            <Link to="/"><i className="fas fa-store"></i></Link>
-            <Link to="/plant"><i className="fas fa-tree"></i></Link>
-            <Link to="/profile"><i className="fas fa-user"></i></Link>
-          </div>
-        </nav> */}
-        <div className="top">
           <img src="../assets/icon.png" alt="Leafbit" />
         </div>
         <Switch>
           <Route exact path="/" component={TreeProgress} />
           <Route path="/market" component={Marketplace} />
+          <Route path="/map" component={TreeMap} />
           <Route path="/profile" component={Marketplace} />
         </Switch>
       </div>
